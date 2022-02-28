25878eac752f792f1f6e6167d40f1adafe8f0d53
Jens Östlund <jens.ostlund@futurice.com>
2022-02-28 22:57:29 +0100

    Move imports to the top of the file
    It's good practice to put imports at the top of your file.

--- examples/01-hello-node/index.js
+++ examples/01-hello-node/index.js
@@ -1,7 +1,7 @@
-console.log("Hello node! \(>O<)/")
-
 // We can use the import syntax since the package type being set to "module" in package.json
 import fs from 'fs';
+
+console.log("Hello node! \(>O<)/")
 
 fs.writeFileSync("test.txt", "Here's some text in a file!");
 const contents = fs.readFileSync("test.txt").toString();
