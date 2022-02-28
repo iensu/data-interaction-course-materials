a63da3832971a961462c6a4ea82949f18e188c54
Jens Östlund <jens.ostlund@futurice.com>
2022-02-28 23:06:57 +0100

    Add an additional module to 01-hello-node

---
+++ examples/01-hello-node/add-meaning.js
@@ -0,0 +1,8 @@
+/**
+ * Adds meaning to a number.
+ * @param {number} x
+ * @returns {number}
+ */
+export const addMeaning = (x) => {
+  return x + 42;
+}
--- examples/01-hello-node/index.js
+++ examples/01-hello-node/index.js
@@ -1,8 +1,13 @@
 // We can use the import syntax since the package type being set to "module" in package.json
 import fs from 'fs';
+import { addMeaning } from './add-meaning.js';
 
 console.log("Hello node! \(>O<)/")
 
 fs.writeFileSync("test.txt", "Here's some text in a file!");
 const contents = fs.readFileSync("test.txt").toString();
 console.log(contents);
+
+const x = addMeaning(0);
+
+console.log(`x=${x}`);
