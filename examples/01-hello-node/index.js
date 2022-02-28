4ccc166095b9496e3151f1925daaca923adc9f3d
Jens Östlund <jens.ostlund@futurice.com>
2022-02-28 22:37:26 +0100

    Add file operation examples using JavaScript modules style imports

--- examples/01-hello-node/index.js
+++ examples/01-hello-node/index.js
@@ -1,1 +1,8 @@
 console.log("Hello node! \(>O<)/")
+
+// We can use the import syntax since the package type being set to "module" in package.json
+import fs from 'fs';
+
+fs.writeFileSync("test.txt", "Here's some text in a file!");
+const contents = fs.readFileSync("test.txt").toString();
+console.log(contents);
