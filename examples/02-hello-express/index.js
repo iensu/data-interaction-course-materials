debed8449e2fde9b26552a1d04cacad17e2b79fd
Jens Östlund <jostlund@gmail.com>
2022-03-02 11:14:12 +0100

    Add another endpoint to 02-hello-express

--- examples/02-hello-express/index.js
+++ examples/02-hello-express/index.js
@@ -6,6 +6,10 @@ app.get('/hello', (req, res) => {
   res.send('Hello there!').end();
 });
 
+// app.get('/another-page', (req, res) => {
+//   res.send('Another page!').end();
+// });
+
 const PORT = 8080;
 
 app.listen(PORT, () => {
