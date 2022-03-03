9aab26601ff967adbfd04214753d36c8edfa8c42
Jens Östlund <jens.ostlund@futurice.com>
2022-03-03 23:27:35 +0100

    Added more to the mongodb intro example project

--- examples/03-mongodb-intro/02-creating-a-collection.js
+++ examples/03-mongodb-intro/02-creating-a-collection.js
@@ -1,23 +1,31 @@
 const getConnectedClient = require("./01-connecting-to-mongodb");
 
+/**
+ * This is an example of an Immediately Invoked Function Expression.
+ * We are declaring and invoking the function at the same time (see
+ * the empty parentheses at the bottom of this file).
+ *
+ * This is a common pattern when you want to use async/await on the
+ * top-level of a script without creating an additional named function
+ * to do so. Note that we are still wrapping everything in a function,
+ * we just don't have to give it a name.
+ *
+ * Read more here: https://developer.mozilla.org/en-US/docs/Glossary/IIFE
+ */
 (async () => {
   const client = await getConnectedClient("mongodb://localhost:27017");
 
   // You don't need to create the database before hand, it will be created
   // as needed when you start adding documents to one of its collections.
-  const db = client.db("temp-db");
+  const db = client.db("mongodb-intro");
 
   // A database is made up of one or more collections of documents. Collections
   // are also created as documents are added to them.
   const collection = db.collection("todos");
 
-  const todo = {
-    task: "Do laundry",
-    isDone: false,
-    createdAt: new Date().toUTCString(),
-  };
+  const todos = require("./todos.json");
 
-  const receipt = await collection.insertOne(todo);
+  const receipt = await collection.insertMany(todos);
 
   console.log("Receipt:", receipt);
 
