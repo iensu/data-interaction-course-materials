9d79f1c782a54ee2f68887b0b2d61ccb38d03114
Fabian Miiro <fabian.miiro@outlook.com>
2022-03-04 20:29:33 +0100

    Add some examples of status codes

--- examples/f/l04-verbs/get.js
+++ examples/f/l04-verbs/get.js
@@ -3,9 +3,22 @@ import fs from "fs";
 
 const app = express();
 
+// GET
 app.get("/get-what", (req, res) => {
   res.send("https://youtu.be/4m48GqaOz90?t=9").end();
 });
+
+// STATUS CODES
+{
+  app.get("/make-world-peace", (req, res) => {
+    res.status(421).send("It's not that simple.").end();
+  });
+  
+  
+  app.get("/download/car", (req, res) => {
+    res.status(507).end();
+  });
+}
 
 const PORT = 8080;
 
