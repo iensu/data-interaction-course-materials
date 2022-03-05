335a7c37045bc15529d10702d69015f3bfa33837
Fabian Miiro <fabian.miiro@outlook.com>
2022-03-05 20:58:34 +0100

    Add possibility to get individual question

--- examples/f/l04-verbs/full_api.js
+++ examples/f/l04-verbs/full_api.js
@@ -27,11 +27,6 @@ class reply {
 
 let questions = [];
 
-app.post('/questions', (req, res) => {
-    questions.push(new question(req.body.title));
-    res.status(200).end();
-});
-
 app.get('/questions', (req, res) => {
     res.json(questions.map((q, index) => {
         return {
@@ -43,27 +38,42 @@ app.get('/questions', (req, res) => {
     }));
 });
 
+app.get('/questions/:id', (req, res) => {
+    let q = questions[req.params.id];
+    res.json({
+        "id": req.params.id,
+        "title": q.title,
+        "average": q.replies.reduce((total, current) => total += current.vote, 0) / q.replies.length,
+        "votes": q.replies.length
+    });
+});
+
+app.post('/questions', (req, res) => {
+    questions.push(new question(req.body.title));
+    res.status(200).end();
+});
+
 app.patch('/questions/:id', (req, res) => {
     let selectedQuestion = questions[req.params.id];
     let voter = req.body.voter;
     let vote = req.body.vote;
 
-    if(voter == undefined) {
+    if (voter == undefined) {
         res.status(400);
         res.write("You need to supply `voter` to vote\n");
     }
-    
-    if(vote == undefined) {
+
+    if (vote == undefined) {
         res.status(400);
         res.write("You need to supply `vote` to vote\n");
     }
 
-    if(res.statusCode != 200) {
+    if (res.statusCode != 200) {
         res.send();
         return;
     }
 
-    if(selectedQuestion == undefined) {
+    if (selectedQuestion == undefined) {
         res.status(404);
         res.send(`That question ${req.params.id} doesn't seem to exist\n`);
         return;
@@ -80,7 +90,10 @@ app.patch('/questions/:id', (req, res) => {
     res.end();
 });
 
-const PORT = 8080;
+if (process.env.PORT == undefined) {
+    process.env.PORT = 8080
+}
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
